2145410409632df1bfdc9240765f198265be0dbe
Ian Wallis <ian.wallis@officedepot.com>
2015-09-21 19:01:20 +0100

    Convert today.js to AMD format

--- public/js/pages/today.js
+++ public/js/pages/today.js
@@ -3,59 +3,79 @@
  */
 
 
-// Some global data
-var SW_PLANS = {
-	RED: {
-		name: 'Red',
-		synLimit: 20,
-		hoaLimit: 1,
-		hobLimit: 1
-	},
-	GREEN: {
-		name: 'Green',
-		synLimit: 20,
-		hoaLimit: 1,
-		hobLimit: 1
-	},
-	EXTRA_EASY: {
-		name: 'Extra Easy',
-		synLimit: 20,
-		hoaLimit: 1,
-		hobLimit: 1
-	},
-	EXTRA_EASY_SPEEDY: {
-		name: 'Extra Easy Speedy',
-		synLimit: 20,
-		hoaLimit: 1,
-		hobLimit: 1
+define(function(require){
+	'use strict';
+
+	var _ = require('lodash'),
+		$ = require('jquery'),
+		mustache = require('mustache');
+
+
+	// Some global data
+	var SW_PLANS = {
+		RED: {
+			name: 'Red',
+			synLimit: 20,
+			hoaLimit: 1,
+			hobLimit: 1
+		},
+		GREEN: {
+			name: 'Green',
+			synLimit: 20,
+			hoaLimit: 1,
+			hobLimit: 1
+		},
+		EXTRA_EASY: {
+			name: 'Extra Easy',
+			synLimit: 20,
+			hoaLimit: 1,
+			hobLimit: 1
+		},
+		EXTRA_EASY_SPEEDY: {
+			name: 'Extra Easy Speedy',
+			synLimit: 20,
+			hoaLimit: 1,
+			hobLimit: 1
+		}
+	};
+
+	var FOOD_CATEGORIES = ['SYNS', 'HOA', 'HOB', 'FREE'];
+
+
+	// example data
+
+	var userData = {
+		totals: {
+			syns: 15,
+			hea: 1,
+			heb: 0,
+		},
+		planType: 'RED',
+
+		foodItems: [
+			new FoodItem('HOA', 'Example Healthy Option A', '20g', 0),
+			new FoodItem('HOB', 'Example Halthy Option B', 10, 0)
+
+		]
+	};
+
+	function FoodItem(category, description, amount, syns){
+		this.category = category || 'SYNS';
+		this.description = description || 'Added food: ' + this.category;
+		this.amount = amount || 0;
+		this.syns = syns || 0;
 	}
-};
-
-var FOOD_CATEGORIES = ['SYNS', 'HOA', 'HOB', 'FREE'];
 
 
-// example data
 
-var userData = {
-	totals: {
-		syns: 15,
-		hea: 1,
-		heb: 0,
-	},
-	planType: 'RED',
+	return {
+		SW_PLANS: SW_PLANS,
+		FOOD_CATEGORIES: FOOD_CATEGORIES,
+		FoodItem: FoodItem
+	};
 
-	foodItems: [
-		new FoodItem('HOA', 'Example Healthy Option A', '20g', 0),
-		new FoodItem('HOB', 'Example Halthy Option B', 10, 0)
+});
 
-	]
-};
 
-function FoodItem(category, description, amount, syns){
-	this.category = category || 'SYNS';
-	this.description = description || 'Added food: ' + this.category;
-	this.amount = amount || 0;
-	this.syns = syns || 0;
-}
 
 
