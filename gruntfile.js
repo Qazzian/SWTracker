d8d287935a9c2d2488e76d52869d12fc132c2e21
Ian Wallis <ian.wallis@officedepot.com>
2015-09-23 18:18:26 +0100

    Add css lint to grunt task

--- gruntfile.js
+++ gruntfile.js
@@ -38,13 +38,23 @@ module.exports = function(grunt) {
 		requirejs: {
 
 		},
+		scsslint: {
+			allFiles: [
+				'public/sass/**/*.scss',
+				'!public/sass/lib/**/*.scss'
+			],
+			options: {
+				config: '.scss-lint.yml',
+				colorizeOutput: true
+			}
+		},
 		watch: {
 			options: {
 				interrupt: true
 			},
 			styles: {
 				files: ['public/sass/**/*.scss'],
-				tasks: ['compass:dev']
+				tasks: ['compass:dev', 'scsslint']
 			}
 		}
 
@@ -53,6 +63,8 @@ module.exports = function(grunt) {
 	// Load plugins.
 	grunt.loadNpmTasks('grunt-bower');
 	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-scss-lint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Custom tasks.
