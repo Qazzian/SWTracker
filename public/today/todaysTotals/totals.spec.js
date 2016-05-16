fe090a5fa308000bb3bbe523763f51f24cc7a993
Qazzian <qazzian@gmail.com>
2016-05-16 06:36:05 +0100

    Fixed unit tests for todays totals component

--- public/today/todaysTotals/totals.spec.js
+++ public/today/todaysTotals/totals.spec.js
@@ -1,6 +1,10 @@
 'use strict';
 
-import {default as totals} from './totals';
+import {default as $} from 'jquery';
+import {default as can} from 'can';
+import {default as Totals} from './totals';
+
+import {default as scratchpad} from 'testing/scratchpad';
 
 var data = {
 	syns: 15,
@@ -8,17 +12,24 @@ var data = {
 	hob: 0
 };
 
+var componentWrapper = '<div id="totalsWrapper"><todaysTotals></todaysTotals></div>';
+
 describe('totals', () => {
+
+	beforeEach(function() {
+		scratchpad.add(componentWrapper);
+	});
 
 	it('should be defined', function() {
 		expect(true).toBe(true);
-		expect(totals).toBeDefined();
+		expect(Totals).toBeDefined();
 	});
 
 	it('should render correctly', function(done) {
-	    var instance = new totals(data);
-		// exp
-	    done();
+		can.view('totalsWrapper');
+		var $totalSection = $('section.totals');
+		expect($totalSection.length).toBeGreaterThan(0);
+		done();
 	});
 });
 
